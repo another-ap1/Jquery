8222ac0f3166183a40a62231e56aa1e1e4c6a2b6
Grant Bollaert <grant.l.bollaert@hotmail.com>
2023-12-11 13:22:56 -0800

    adding files

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ $('#submit').on('click', function(e){
     e.preventDefault();
     //checks if the movie title length is more than 2 and the rating is 1-10
     if($('#movie').val().length >= 2 && $('#rating').val() >= 0 && $('#rating').val() <= 10){
-        $('#movieRatingList').append('<li class="movieEntry">'+'Movie:'+$('#movie').val()+" "+' Rating:'+$('#rating').val()+'<button class="rmv">Remove</button>'+'</li>');
+        $('#movieRatingList').append('<li class="movieEntry">'+'<b>Movie: </b>'+$('#movie').val()+"---"+'<b>Rating:</b>'+$('#rating').val()+'<button class="rmv">Remove</button>'+'</li>');
         $('#movie').val("");
         $('#rating').val("");
-    };
-    //if conditions are not met send this alert
-    alert("movie must contain atleast 2 letters and rating must be 1-10");
+    } else {
+        alert("movie must contain atleast 2 letters and rating must be 1-10");
+    }
 });
 
 //adding event listner to each new list item
